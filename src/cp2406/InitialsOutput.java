d880f379af325994f41af6a8b54c7bca92549302
Cameron Attard <cameron.attard@my.jcu.edu.au>
2021-08-13 16:14:18 +1000

    Complete InitialsOutput class

---
+++ src/cp2406/InitialsOutput.java
@@ -0,0 +1,15 @@
+package cp2406;
+
+public class InitialsOutput {
+
+    public static void main(String[] args) {
+	    System.out.println("      *******              ***             ***       ***   ");
+        System.out.println("    **       **           ** **            ** **   ** **   ");
+        System.out.println("   **                    **   **           **  ** **  **   ");
+        System.out.println("   **                   **     **          **   **    **   ");
+        System.out.println("   **                  ***********         **         **   ");
+        System.out.println("   **                 **         **        **         **   ");
+        System.out.println("    **       **      **           **       **         **   ");
+        System.out.println("      *******       **             **      **         **   ");
+    } // end main()
+} // end class
